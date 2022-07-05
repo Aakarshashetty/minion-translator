b0c6f997351615382f691f44a9a536fe8e9244db
Aakarshashetty <aakarshakodthiwada61121@gmail.com>
2022-07-05 15:13:43 +0530

    write a div to show output

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 console.log("Hii!");
 var btnTranslator = document.querySelector("#btn-translator");
+var txtInput = document.querySelector("#txt-input");
+var output = document.querySelector(".output");
+
 btnTranslator.addEventListener("click", function buttonClicked(){
     console.log("Button Clickled!");
+    // var input = console.log("input",txtInput.value);
+    output.innerText = "Hii "+txtInput.value;
 });
